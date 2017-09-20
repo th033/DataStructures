53b1b0530fdbd88c87b0ba35ff3e9d2ad1062666
th033 <31709788+th033@users.noreply.github.com>
2017-09-19 22:20:10 -0500

    Add files via upload

---
+++ HockettP2DS/src/LinkList.java
@@ -0,0 +1,22 @@
+/**
+     * Class Name: LinkList
+     * Class Author: Tyler Hockett
+     * **************************
+     * Purpose of the Class
+     * to create a link list/to 
+     * to store 500 in values to search
+     * through
+     * **************************
+     * Date: 9/19/2017 USA
+     * **************************
+     * Date Changed:
+     * **************************
+     * Look At This!
+     * 
+     * **************************
+     */
+public class LinkList 
+{
+    int value;
+    LinkList nextLL;  
+}
